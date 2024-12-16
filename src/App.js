004192d20f08b74b8922c9918725f66cef8867b4
LaviRastogi <1993lavirastogi@gmail.com>
2024-12-16 21:23:44 +0530

    useOptimisticUpdate

--- src/App.js
+++ src/App.js
@@ -5,24 +5,17 @@ import DefferedValueExample from "./DefferedValueExample";
 import RefExample from "./RefExample";
 import { ThemeProvider } from "./ThemeContext";
 
+import Counter from "./UseOptimisticExample";
+
 function App() {
   return (
     <ThemeProvider>
       <title>React 19 application</title>
       <div className="App">
         {/* ref */}
-        <h1>React Ref Example</h1>
-        <RefExample />
-        {/* context API */}
-        <h1>React Context API Example</h1>
-        <ContextAPIExample />
-        {/* defferedValueexample */}
-        <DefferedValueExample />
-        <h1>New hook</h1>
-        {/* action and useFormSatus */}
-        <h1>Form Example</h1>
-        <FormChangeExample />
-        {/* useTransition */}
+
+        {/* useOptimistic */}
+        <Counter />
       </div>
     </ThemeProvider>
   );
---
+++ src/UseOptimisticExample.js
@@ -0,0 +1,44 @@
+import { useOptimistic, useState, useRef } from "react";
+
+async function updateTodoAPP(message) {
+  await new Promise((res) => setTimeout(res, 4000));
+  return message;
+}
+function Thread({ tasks, sendTask }) {
+  const formRef = useRef();
+  async function formAction(formData) {
+    addOptimisticMessage(formData.get("task"));
+    formRef.current.reset();
+    await sendTask(formData);
+  }
+  const [optimisticMessages, addOptimisticMessage] = useOptimistic(
+    tasks,
+    (state, newTask) => [
+      ...state,
+      {
+        text: newTask,
+      },
+    ]
+  );
+
+  return (
+    <>
+      <form action={formAction} ref={formRef}>
+        <input type="text" name="task" placeholder="Enter task" />
+        <button type="submit">Submit</button>
+      </form>
+      {optimisticMessages.map((task, index) => (
+        <div key={index}>{task.text}</div>
+      ))}
+    </>
+  );
+}
+
+export default function Counter() {
+  const [todoApp, setTodoAPP] = useState([{ text: "Hello Complete JS!" }]);
+  async function sendTask(formData) {
+    const updatedTask = await updateTodoAPP(formData.get("task"));
+    setTodoAPP(() => [...todoApp, { text: updatedTask }]);
+  }
+  return <Thread tasks={todoApp} sendTask={sendTask} />;
+}
